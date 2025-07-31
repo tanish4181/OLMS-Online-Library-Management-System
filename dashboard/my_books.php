9d4f81dbd9665407ffe3c138b3bb935d49ff6b94
Mehulsuthar817 <mehulsuthar817@gmail.com>
2025-08-01 01:37:31 +0530

    Implement user login functionality with session management and error handling; refactor navbar into separate header file.

--- dashboard/my_books.php
+++ dashboard/my_books.php
@@ -12,45 +12,7 @@
   </head>
   <body class="user-dashboard my_books">
     <!-- Navbar -->
-    <nav
-      class="navbar navbar-expand-lg"
-      style="background: linear-gradient(to right, #6a89ec, #7e5ce4)"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand fw-bold text-white" href="./user_dashboard.html">OLMS</a>
-
-        <button
-          class="navbar-toggler text-white"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse user-dnav" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link text-white" href="./user_dashboard.html"
-                >Home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="./browse_book.html">Browse Books</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="./my_books.html">My Books</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="../about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <button class="btn btn-secondary">Logout</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<?php include("./user_header.php"); ?>
 
     <!-- my books box -->
     <div class="mBook-main-box">
