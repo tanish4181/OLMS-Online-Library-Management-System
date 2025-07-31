7e8c160a19fab7de81b4a0919227a8327e800038
Tanish4181 <167396820+tanish418@users.noreply.github.com>
2025-07-31 10:21:10 +0530

    change admin navbar

--- Admin_dashboard/navbar_admin.php
+++ Admin_dashboard/navbar_admin.php
@@ -23,19 +23,13 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse user-dnav" id="navbarNav">
+            <div class="collapse navbar-collapse user-dnav d-flex justify-content-between align-items-center" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="./admindashboard.html">Home</a>
+                        <a class="nav-link text-white" href="/OLMS/Admin_dashboard/admindashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="#">Browse Books</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">My Books</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="/OLMS/Admin_dashboard/admindashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="#">Manage books</a>
@@ -43,13 +37,11 @@
                     <li class="nav-item">
                         <a class="nav-link text-white" href="/olms/Admin_dashboard/Manage_user.php">Manage users</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btn-secondary">Logout</button>
-                    </li>
                 </ul>
+                <div class="logout-button">
+                    <button class="btn btn-secondary">Logout</button>
+
+                </div>
             </div>
         </div>
     </nav>
