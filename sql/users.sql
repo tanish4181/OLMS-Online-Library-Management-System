1e8738fc9524cce2fbf34e4abe99f56dfe2a313c
Mehulsuthar817 <mehulsuthar817@gmail.com>
2025-08-02 16:37:08 +0530

    add books sql

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2025 at 03:04 PM
+-- Generation Time: Aug 02, 2025 at 01:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,37 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `books`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `books` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `address` text NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(10) DEFAULT 'user'
+  `title` varchar(255) DEFAULT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `books`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `fullname`, `address`, `password`, `role`) VALUES
-(1, 'test1', 'test1@gmail.com', 'test1', 'jaipur', '$2y$10$86uG.EW.o9qPPTmDsia.Qut53xfYTOSNGLOs.5YWsSl3XXm.oveXu', 'user'),
-(2, 'test1', 'test1@gmail.com', 'test1', 'jaipur', '$2y$10$Abiw8TvTcyv5k2gA4kIG1ODMKdMO6nMSKeOHabkAsTi5x/wEdzNQu', 'user'),
-(3, 'test2', 'test2@gmail.com', 'test2', 'jaipur', '$2y$10$MjWUziPLqIa8vguvwfqSe.0bK0tdlZeDuFXM8wHvzZmCQy57h2Kqe', 'user'),
-(5, 'admin1', 'admin@gmail.com', 'Admin User', 'Admin Address', '$2y$10$2Klq0md1RHqFMgS4g6pmcOpOfcY.8hd8LbBtNT5ZyWH7Oxkd/e0Le', 'admin');
+INSERT INTO `books` (`id`, `title`, `cover`, `author`, `category`, `quantity`, `description`) VALUES
+(1, 'test', '../uploads/920192.jpg', 'Jane Doe', 'novel', 5, 'test');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `books`
 --
-ALTER TABLE `users`
+ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -62,10 +59,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `books`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
