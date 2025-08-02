6af88568c1df7d24e3344000bb444289e76dd1c8
Tanish4181 <167396820+tanish418@users.noreply.github.com>
2025-08-02 23:26:35 +0530

    remove about.html in userdashboard

--- dashboard/user_header.php
+++ dashboard/user_header.php
@@ -1,7 +1,6 @@
-    <nav
-      class="navbar navbar-expand-lg bg-primary"
-      
-    >
+   
+   <nav
+      class="navbar navbar-expand-lg bg-primary">
       <div class="container-fluid">
         <a class="navbar-brand fw-bold text-white" href="./user_dashboard.php">OLMS</a>
 
@@ -9,17 +8,14 @@
           class="navbar-toggler text-white"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
+          data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse user-dnav" id="navbarNav">
+        <div class="collapse navbar-collapse user-dnav d-flex justify-content-between align-items-center" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link text-white" href="./user_dashboard.php"
-                >Home</a
-              >
+              <a class="nav-link text-white" href="./user_dashboard.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-white" href="./browse_books.php">Browse Books</a>
@@ -30,13 +26,11 @@
             <li class="nav-item">
               <a class="nav-link text-white" href="./my_books.php">My Books</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="about.php">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="../logout.php"><button class="btn btn-secondary"> Logout </button></a> 
-            </li>
           </ul>
+          <div class="logout-button">
+            <a href="../logout.php"><button class="btn btn-secondary"> Logout </button></a>
+
+          </div>
         </div>
       </div>
     </nav>
